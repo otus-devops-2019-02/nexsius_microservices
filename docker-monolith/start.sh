b732a5da47f3c8637d50e289349bb8e4ee5e97f9
Sergey Nex <nexsius@gmail.com>
2019-05-06 16:12:07 +0300

    fix mongo start

--- docker-monolith/start.sh
+++ docker-monolith/start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-/usr/bin/mongod --fork --logpath /var/log/mongod.log --config /
-etc/mongodb.conf
+/usr/bin/mongod --fork --logpath /var/log/mongod.log --config /etc/mongodb.conf
 source /reddit/db_config
 cd /reddit && puma || exit
